c678261e3b823f9106254d48b4707f04d07a1b1e
Stephen Mwangi <mwangiwahome70@gmail.com>
2024-09-13 11:28:14 +0300

    Updated products data

--- JS/ASSIGNMENT4/tasks.js
+++ JS/ASSIGNMENT4/tasks.js
@@ -5,9 +5,7 @@ document.addEventListener ('DOMContentLoaded', () => {
   // Fetch products when button is clicked
   fetchDataButton.addEventListener ('click', () => {
     fetch ('http://localhost:8085/products')
-      .then (response => {
-        response.json ();
-      })
+      .then (response => response.json ())
       .then (products => {
         // Clear the existing product list
         productList.innerHTML = '';
@@ -15,23 +13,28 @@ document.addEventListener ('DOMContentLoaded', () => {
         // Loop through the products and display each one
         products.forEach (product => {
           const productItem = document.createElement ('div');
-          productItem.classList.add ('productList');
+          productItem.classList.add ('productItem');
 
           const productName = document.createElement ('div');
+          const productDescription = document.createElement ('div');
           const productPrice = document.createElement ('div');
-          const productColor = document.createElement ('div');
-          const productAvailable = document.createElement ('div');
+          const productCategory = document.createElement ('div');
+          const productStock = document.createElement ('div');
 
-          productName.textContent = `${product.name}`;
-          productPrice.textContent = `$${product.price}`;
-          productColor.textContent = `${product.color}`;
-          productAvailable.textContent = `${product.isAvailable ? 'Available' : 'Out of Stock'}`;
+          productName.textContent = `Name: ${product.name}`;
+          productDescription.textContent = `Description: ${product.description}`;
+          productPrice.textContent = `Price: $${product.price}`;
+          productCategory.textContent = `Category: ${product.category}`;
+          productStock.textContent = `Stock: ${product.stock}`;
 
+          // Append all product details to the product item
           productItem.appendChild (productName);
-          productItem.appendChild (productColor);
+          productItem.appendChild (productDescription);
           productItem.appendChild (productPrice);
-          productItem.appendChild (productAvailable);
+          productItem.appendChild (productCategory);
+          productItem.appendChild (productStock);
 
+          // Append the product item to the product list
           productList.appendChild (productItem);
         });
       })
